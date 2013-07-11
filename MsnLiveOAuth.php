f628ea53cd9f2ac65661fe7f2dd7dcf5cb98702a
Juan Carlos Juarez <jjuarez@jobomas.com>
2013-07-11 17:08:24 -0500

    Starting repo

---
+++ MsnLiveOAuth.php
@@ -0,0 +1,57 @@
+<?php
+
+class MsnLiveOAuth {
+    
+    private $clientId;
+    private $clientSecret;
+
+    const AUTH_URL = 'https://login.live.com/oauth20_authorize.srf';
+    const AUTH_TOKEN_URL = 'https://login.live.com/oauth20_token.srf';
+    const CONTACTS_SERVICE_URL = 'https://apis.live.net/v5.0/%s/%s';
+
+    public function __construct($clientId, $clientSecret) {
+        $this->clientId = $clientId;
+        $this->clientSecret = $clientSecret;
+    }
+
+    public function GetAccessCode($scope, $responseType, $redirectUrl='') {
+        $url = self::AUTH_URL .
+                '?client_id='. $this->clientId .
+                '&scope=' . rawurlencode($scope) .
+                '&response_type=' . $responseType .
+                (!empty($redirectUrl) 
+                    ? '&redirect_url=' . urlencode($redirectUrl) : '');
+        header('Location: ' . $url);
+    }
+
+    public function ValidateCodeAndGetTokenAccess($code, $grantType='authorization_code') {
+        $postData = array(
+            'client_id' => $this->clientId,
+            'client_secret' => $this->clientSecret,
+            'code' => $code,
+            'grant_type' => $grantType,
+        );
+        $response = $this->CallPostBack(self::AUTH_TOKEN_URL, $postData);
+        return json_decode($response, true); 
+    }
+
+    public function GetContacts($accessToken, $userId='me') {
+        $contacts = file_get_contents(sprintf(self::CONTACTS_SERVICE_URL, 
+            $userId, 'contacts') . '?access_token=' . $accessToken);
+        return json_decode($contacts, true);
+    }
+
+    private function CallPostBack($url, $postData) {
+        $data = http_build_query($postData);
+        $opts = array(
+            'http' => array(
+                'method' => 'POST',
+                'header' => 'Content-type: application/x-www-form-urlencoded;charset=UTF-8',
+                'content' => $data,
+            )
+        );
+        $context = stream_context_create($opts);
+        $result = file_get_contents($url, false, $context);
+        return $result;
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+Example
+
+<?php
+include 'MsnLiveOAuth.php';
+
+$msnAuth = new MsnLiveOAuth('YOUR_CLIENT_ID', 'YOUR_CLIENT_SECRET');
+if (!isset($_GET['code'])) {
+    $scope = 'wl.signin wl.basic wl.contacts_emails';
+    $msnAuth->GetAccessCode($scope, 'code');
+} else {
+    $accessInfo = $msnAuth->ValidateCodeAndGetTokenAccess($_GET['code']);
+    if (isset($accessInfo['access_token'])) {
+       $contacts = $msnAuth->getContacts($accessInfo['access_token']);
+       print_r($contacts);
+    } else {
+        echo "Unahutorized";
+    }
+}
